481de91bc37899de89107e144a0fef7fb2f04bef
Terkild Brink Buus <terkild.buus@sund.ku.dk>
2020-08-28 12:52:43 +0200

    Added documentation

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -4,13 +4,25 @@
 #'
 #' Considered empty if equal to "" or "None"
 #'
+#' @param x Input vector
+#' @param empty.values  Vector of values considered "empty"
+#'
+#' @return Vector where empty values have been removed
+#' @export
 
 no_empty <- function(x, empty.values=c("","None")){x[!x %in% empty.values]}
 
 
 #' Make VDJ string
 #'
-#' Paste values of multiple columns into a single string
+#' Concatenate values of multiple columns into a single string after removing empty values by \code{no_empty}
+#'
+#' @param x vector of values
+#' @param sep seperator in concatenated string
+#'
+#' @return Concatenated string where empty values have been removed
+#' @export
+
 paste_vdj <- function(x, sep=":"){
 	paste(apply(x,1,function(y){
 		paste(no_empty(y),collapse=sep)
